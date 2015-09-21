cf0d30d869bcff879b0ccf08af7f5fcbedfbd6d8
John T Knox <JTKnox@Johns-MacBook-Pro.local>
2015-09-21 13:24:47 -0700

    .escape passes tests

--- Spec/RegularEnglishSpec.js
+++ Spec/RegularEnglishSpec.js
@@ -1,18 +1,25 @@
 var specs = [
-{
-  assertion: "Should create global RegEng constructor",
+{ assertion: "Should create global RegEng constructor",
   actual: RegEng,
-  relation: "to be an",
-  expect: "object",
+  relation: "to be a",
+  expect: "function",
   test: function() {
-    return typeof actual === "function";
-  }
-}
+    return typeof this.actual === this.expect;
+  },
+},
+{ assertion: "Should escape special characters",
+  actual: RegEng().escape("\\^${}[]().*+?<>"),
+  relation: "to equal",
+  expect: "\\\\\\^\\$\\{\\}\\[\\]\\(\\)\\.\\*\\+\\?\\<\\>",
+  test: function() {
+    return this.actual === this.expect;
+  },
+},
 ];
 
 for (var i = 0; i < specs.length; i++) {
   var spec = specs[i];
-  if (spec.test()) {
+  if (!spec.test()) {
     console.log(
       spec.assertion, "Falied\nExpected",
       spec.actual, spec.relation, spec.expect
