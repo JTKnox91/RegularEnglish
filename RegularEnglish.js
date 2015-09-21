9104aa85514b9e05225b4eaffec56b4fa7dd467c
John T Knox <JTKnox@Johns-MacBook-Pro.local>
2015-09-21 12:20:45 -0700

    Added .make method

--- RegularEnglish.js
+++ RegularEnglish.js
@@ -1,6 +1,27 @@
-window.RegEng = function(str) {
+window.RegEng = function(flags) {
   newInstance = Object.create(RegEngMethods);
-  newInstance.self = "";
+  newInstance.current = "";
   newInstance.groups = [];
+  newInstance.fromStart = false;
+  newInstance.toEnd = false;
   return newInstance;
+};
+
+var RegEngMethods = {
+  make: function () {
+    //TODO: include error handling for invalid expressions
+    var regExpStr = this.fromStart ? "^" : "";
+    if (this.groups.length) {
+      if (this.current.length) {
+        this.groups.push(this.current);
+      }
+      regExpStr += "(" + this.groups.join(")(") + ")";
+    }
+    else {
+      regExpStr += this.current;
+    }
+    regExpStr += this.toEnd ? "$" : "";
+    return new RegExp(regExpStr);
+  },
+
 };
